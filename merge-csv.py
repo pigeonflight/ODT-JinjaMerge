948981b47f7635bd108e695ed1ac45513f762d25
David Bain <pigeonflight@gmail.com>
2011-05-03 11:17:07 -0500

    added two new files

---
+++ merge-csv.py
@@ -0,0 +1,29 @@
+import csv,cgi
+from jinja2 import FileSystemLoader,Environment
+env = Environment(loader=FileSystemLoader('./'))
+
+data = open('data.csv')
+reader = csv.DictReader(data)
+
+skipped_ids = ('GRA01',
+'GRA02',
+'GRA03',
+'GRA04',
+'GRA05',
+'GRA06',
+'GRA07',
+'GRA08',
+'GRA09',
+'GRA10')
+
+users =  [user for user in reader if user['ID1'] not in skipped_ids]
+newusers = []
+for user in users:
+    newuser = {}
+    for key in user.keys():
+        if key == 'Photo':
+            user[key] = user['Last_Name'].lower() +  user['First_Name'][:1].lower() 
+        newuser[key] = unicode(cgi.escape(user[key]), errors='ignore')
+    newusers.append(newuser)
+template = env.get_template('content4.xml.tmpl')
+print template.render(users=newusers)
